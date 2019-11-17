256fd293fc02d548fd9f80839a74a489c544ac62
FajrulHidayat <38638257+FajrulHidayat@users.noreply.github.com>
2019-11-17 17:51:49 +0800

    Revert "Merge branch 'master' of https://github.com/mrum13/Proyek-Client_Server_Absen"
    This reverts commit 7ed380b186ae38e3da67c689501b8b438a54fb45, reversing
    changes made to e584214fc5810af96163003caadf264fc0ac7f48.

--- say-hello-api/src/com/echo/clientserver/sayhello/SayHello.java
+++
@@ -1,9 +0,0 @@
-package com.echo.clientserver.sayhello;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface SayHello extends Remote{
-    
-    public String sayHello(String nama) throws RemoteException;
-}
